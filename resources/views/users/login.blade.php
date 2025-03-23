bcaf7a9f981d412ee8f36b2160e2ffca24183a9d
fpmukesh <mukesh@fpdemo.com>
2025-03-23 16:39:29 +0530

    Login update new v2 23 March

--- resources/views/users/login.blade.php
+++ resources/views/users/login.blade.php
@@ -2,11 +2,11 @@
 @section('content')
 <!-- <p>You will find this file in resources/views/users/login.blade.php</p> -->
 <div class="flex flex-wrap lg:flex-nowrap justify-center w-[100%] max-[1920px] items-stretch gap-[0]">
-    <div class="flex flex-wrap md:flex-nowrap justify-center w-[100%] lg:w-[45%] max-w-[854px]  px-[20px] py-[50px] md:px-[30px] md:py-[60px]  lg:px-[20px] lg:py-[65px]   ">
-        <div class="max-w-[450px] w-[100%]">
-            <div class="logo flex items-center justify-center mb-[40px] bg-[#090B13] px-[15px] py-[25px] md:mb-[50px] lg:mb-[80px] xl:mb-[100px]">
+    <div class="loginBx flex items-center flex-wrap md:flex-nowrap justify-center w-[100%] px-[20px] py-[50px] md:px-[30px] md:py-[60px]  lg:px-[20px] lg:py-[65px]   ">
+        <div class="max-w-[450px] w-[100%] px-[20px] py-[35px] md:px-[40px] md:py-[65px] bg-[#fff] rounded-[10px] md:rounded-[15px]">
+        <div class="logo1 flex items-center justify-center mb-[20px] md:mb-[50px]">
                 <a href="#">
-                    <img src="images/logo.png" alt="">
+                    <img src="images/logo-offerwall-login.png" alt="logo">
                 </a>
             </div>
             <script>
@@ -18,7 +18,7 @@
                     toastr.error("{{ session('error') }}");
                 @endif
             </script>
-            <h2 class="text-[25px] leading-[30px] md:text-[28px] md:leading-[30px] lg:text-[30px] lg:leading-[32px] font-[700] text-[#1A1A1A]"> Log in to get started!!  </h2>
+            <h2 class="text-[25px] leading-[30px] md:text-[28px] md:leading-[30px] lg:text-[30px] lg:leading-[32px] font-[700] text-[#1A1A1A] text-center"> Log in to get started! mks </h2>
             <div class="mt-[30px] md:mt-[45px] lg:mt-[55px]">
                 <form method="post">
                     @csrf
@@ -64,10 +64,10 @@
                     </div>
                 </div> 
                 <div class="text-right mt-[8px]">
-                    <a id="resetPass" href="#" class="text-[14px] text-[#D272D2] font-[500]" onclick="resetPass()">Reset password</a>
+                    <a id="resetPass" href="#" class="text-[14px] text-[#4EF953] font-[500]" onclick="resetPass()">Reset password</a>
                 </div>--}}
                     <div class="mt-[30px]">
-                        <button type="submit" class="w-[100%] bg-[#D272D2] px-[10px] py-[15px] text-[18px] text-[#fff] font-[500] text-center rounded-[8px] hover:bg-[#000]">
+                        <button type="submit" class="w-[100%] bg-[#4EF953] px-[10px] py-[15px] text-[18px] text-[#210D0F] font-[500] text-center rounded-[8px] hover:bg-[#000] hover:text-[#fff]">
                         Login
                         </button>
                     </div>
@@ -77,18 +77,16 @@
                     <div class="text-[14px] font-[500] text-[#898989]">OR</div>
                     <div class="w-[48%] h-[1px] bg-[#ccc]"></div>
                 </div>
-                <div class="mt-[50px] text-[14px] font-[500] text-[#898989]">
+                <div class="mt-[50px] text-[14px] font-[500] text-[#898989] text-center">
                 Don’t have an account? 
-                    <a href="#" class="text-[#D272D2] underline hover:text-[#000] hover:no-underline ">
+                    <a href="#" class="text-[#4EF953] underline hover:text-[#000] hover:no-underline ">
                     Sign Up
                     </a>
                 </div>
             </div>
         </div>
     </div>
-    <div class="w-[100%]  lg:w-[55%] max-w-[1070px]">
-        <img class="w-[100%] max-w-[100%] h-[100%] " src="images/loginbanner.jpg" alt="">
-    </div>
+   
 </div>
 <script>
     function resetPass() {

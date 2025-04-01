04abbe2e1969f98e0d401bad5c3c578a8ef92aa5
Rahul Chouhan <r.chouhan64@gmail.com>
2025-04-01 17:46:14 +0530

    new change

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -2,14 +2,13 @@
 @section('content')
 <div class="px-[15px] py-[15px]  md:px-[20px] md:py-[20px] lg:px-[30px] lg:py-[30px]">
     <div class="flex flex-wrap md:flex-nowrap items-center gap-[15px] mb-[30px]">
-        <div class="pinkbg flex flex-col justify-center  items-start gap-[5px] w-[100%] sm:w-[150px] md:w-[180px] lg:w-[200px] xl:w-[300px] 2xl:w-[365px] h-[130px]  rounded-[7px] lg:rounded-[10px] px-[15px] py-[15px] md:px-[20px] md:py-[20px] lg:px-[30px] lg:py-[30px] activeApps">
-            <h2 class="text-[18px] font-[500] text-[#000]">Active Apps</h2>
-            <h3 class="text-[38px] font-[700] text-[#000]">{{ $activeApps ?? 0 }}</h3>
+        <div class="graybg flex flex-col justify-center bg-[#C855C8] items-start gap-[5px] w-[100%] sm:w-[200px] md:w-[265px] lg:w-[365px] h-[130px]  rounded-[7px] lg:rounded-[10px]  px-[15px] py-[15px] md:px-[20px] md:py-[20px] xl:px-[30px] xl:py-[30px] activeApps">
+            <h2 class="text-[18px] font-[500] text-[#080C0F]">Active Apps</h2>
+            <h3 class="text-[25px] lg:text-[25px] xl:text-[38px] lg:text-[25px] xl:text-[38px] font-[700] text-[#080C0F]">{{ $activeApps ?? 0 }}</h3>
         </div>
-
-        <div class="orangebg flex flex-col justify-center bg-[#EF7947] items-start gap-[5px]  w-[100%] sm:w-[200px] md:w-[265px] lg:w-[365px] h-[130px]  rounded-[7px] lg:rounded-[10px] px-[30px] py-[30px] activeApps">
+        <div class="pinkbg flex flex-col justify-center bg-[#4EF953] items-start gap-[5px] w-[100%] sm:w-[200px] md:w-[265px] lg:w-[365px] h-[130px]  rounded-[7px] lg:rounded-[10px]  px-[15px] py-[15px] md:px-[20px] md:py-[20px] xl:px-[30px] xl:py-[30px] activeApps">
             <h2 class="text-[18px] font-[500] text-[#000]">Revenue</h2>
-            <h3 class="text-[38px] font-[700] text-[#000]">$ {{ $totalPayouts ?? 0 }}</h3>
+            <h3 class="text-[25px] lg:text-[25px] xl:text-[38px] font-[700] text-[#000]">$ {{ $totalPayouts ?? 0 }}</h3>
         </div>
     </div>
 
